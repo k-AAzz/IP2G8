f538346964f6482368ef63d6d4df0f65b6744464
Jack Lawrence <jacklawrence27@icloud.com>
2024-02-20 14:08:23 +0000

    Item Chooser [First Pass]
    Put in a pedastool and a item script with some placeholder sprites. Press space to spawn one on the pedastool

---
+++ VideoGame/Assets/Scripts/Item/ItemChooser.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChooser : MonoBehaviour
+{
+    [Header("Item Arrays")]
+    public Sprite[] commonItems;
+    public Sprite[] rareItems;
+    public Sprite[] epicItems;
+    public Sprite[] legendaryItems;
+
+    [Header("References")]
+    public GameObject spawnLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnRandomItem();
+        }
+    }
+
+    void SpawnRandomItem()
+    {
+        //Generate a random value between 0 -> 100
+        int randomValue = Random.Range(0, 100);
+
+        if (randomValue <= 69)
+        {
+            SpawnItem(commonItems);
+        }
+        else if (randomValue >= 70 && randomValue <= 85 )
+        {
+            SpawnItem(rareItems);
+        }
+        else if (randomValue >= 86 && randomValue <= 95)
+        {
+            SpawnItem(epicItems);
+        }
+        else
+        {
+            SpawnItem(legendaryItems);
+        }
+
+        Debug.Log(randomValue);
+    }
+
+    void SpawnItem(Sprite[] itemsArray)
+    {
+        if (itemsArray != null && itemsArray.Length > 0)
+        {
+            int randomIndex = Random.Range(0, itemsArray.Length);
+            Sprite chosenItem = itemsArray[randomIndex];
+
+            //Create the item above the pedastool
+            GameObject newItem = new GameObject("NewItem");
+
+            Vector3 offset = new Vector3(0, 0.8f, 0);
+
+            newItem.transform.position = spawnLocation.transform.position;
+            newItem.transform.position += offset;
+
+
+            //Add a sprite renderer with values
+            SpriteRenderer spriteRenderer = newItem.AddComponent<SpriteRenderer>();
+            spriteRenderer.sprite = chosenItem;
+            spriteRenderer.sortingOrder = 2;
+
+            //Change the scale of the sprite / My references are too small can be removed later on
+            float scaleMultiplier = 2f;
+            newItem.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, 1.0f);
+
+            Debug.Log("Item: " + chosenItem.name);
+        }
+        else
+        {
+            Debug.Log("No item array is empty");
+        }
+    }
+}
