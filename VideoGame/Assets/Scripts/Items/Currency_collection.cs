b925f1566f303590adf357d444ce148768aea762
Gretelandcat <yukkuri142@gmail.com>
2024-03-14 12:18:38 +0000

    currency update

--- VideoGame/Assets/Scripts/Items/Currency_collection.cs
+++ VideoGame/Assets/Scripts/Items/Currency_collection.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Currency_collection : MonoBehaviour
 {
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        //    audioSource = GetComponent<AudioSource>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.transform.tag == "Player")
+        {
+          
+            Destroy(gameObject);
+            //    audioSource.Play();
+        }
     }
 }
